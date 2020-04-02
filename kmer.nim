c7525021ae31f4021a43adae515555c30dc0e2da
Brent Pedersen <bpederse@gmail.com>
2020-04-02 10:06:00 -0600

    start of longer kmers

--- kmer.nim
+++ kmer.nim
@@ -12,6 +12,7 @@
 #       1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 3,
 #       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1]
 
+
 const lookup: array[256, uint64] = [
   0'u64, 1, 2, 3,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
   4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
@@ -33,66 +34,78 @@ const lookup: array[256, uint64] = [
 
 const reverse_lookup* = "ACGTN"
 
-proc encode*(k: string): uint64 {.inline.} =
-  ## encode a string into a uint64
-  when not defined(danger):
-    if len(k) > 31:
-      quit "[kmer] can't encode kmer longer than 31"
-  for c in k:
-    result = (result or lookup[cast[uint8](c)]) shl 2
-  result = result shr 2
+type kmer* = array[2, uint64]
 
+proc `<`*(f, r:kmer): bool {.inline.} =
+  return f[0] < r[0] or (f[0] == r[0] and f[1] < r[1])
 
-proc decode*(e: uint64, k: var string) {.inline.} =
-  ## decode a string from a uint64 into k. the length
-  ## of k determines how much is decoded
-  var
-    base: int
-    i = k.len
-    L = i
-  while i > 0:
-    base = int((e shr (i * 2  - 2)) and 3'u64)
-    k[L-i] = reverse_lookup[base]
-    i -= 1
+proc min*(f, r:kmer): kmer {.inline.} =
+  if f[0] < r[0]: return f
+  if r[0] < f[0]: return r
+  if f[1] <= r[1]: return f
+  return r
 
-proc reverse_complement*(encoded: uint64, L:int|uint64): uint64 {.inline.} =
-  ## fast reverse complement of encoded value
-  # from Zev: https://www.biostars.org/p/113640/#424280
-  result = not encoded
-  result = ((result shr 2'u64 and 0x3333333333333333'u64) or (result and 0x3333333333333333'u64) shl 2'u64);
-  result = ((result shr 4'u64 and 0x0F0F0F0F0F0F0F0F'u64) or (result and 0x0F0F0F0F0F0F0F0F'u64) shl 4'u64);
-  result = ((result shr 8'u64 and 0x00FF00FF00FF00FF'u64) or (result and 0x00FF00FF00FF00FF'u64) shl 8'u64);
-  result = ((result shr 16'u64 and 0x0000FFFF0000FFFF'u64) or (result and 0x0000FFFF0000FFFF'u64) shl 16'u64);
-  result = ((result shr 32'u64 and 0x00000000FFFFFFFF'u64) or (result and 0x00000000FFFFFFFF'u64) shl 32'u64);
-  return (result shr (2 * (32 - L)));
+template mask(k:int): uint64 =
+  (1'u64 shl k) - 1
 
-proc forward_add*(encoded: var uint64, base: char, L: int) {.inline.} =
+proc forward_add*(encoded: var kmer, base: char, L: int) {.inline.} =
   ## drop the first base from an encoded kmer of length L and add a new one.
   ## useful for sliding along a sequence.
-  encoded = (encoded shl 2) and ((1'u64 shl (2*L)) - 1)
-  encoded = (encoded or lookup[cast[int](base)])
+  let mask = L.mask
+  let c = lookup[cast[uint8](base)]
+  encoded[0] = ((encoded[0] shl 1) or (c and 1)) and mask
+  encoded[1] = ((encoded[1] shl 1) or (c shr 1)) and mask
 
-proc reverse_add*(rencoded: var uint64, base: char, L: int) {.inline.} =
+proc encode*(k: string): kmer {.inline.} =
+  ## encode a string into a uint64
+  let mask = k.len.mask
+  when not defined(danger):
+    if len(k) > 63:
+      quit "[kmer] can't encode kmer longer than 31"
+  for base in k:
+    let c = lookup[cast[uint8](base)]
+    result[0] = ((result[0] shl 1) or (c and 1)) and mask
+    result[1] = ((result[1] shl 1) or (c shr 1)) and mask
+
+proc reverse_add*(rencoded: var kmer, base: char, L: int) {.inline.} =
   ## drop the last base from a rev-comped kmer of length L and add a new
   ## complemented base at the start.
-  rencoded = (rencoded shr 2)
-  var tmp = 3'u64 - lookup[cast[int](base)]
-  tmp = tmp shl (L*2-2)
-  rencoded = rencoded or tmp
+  let c = lookup[cast[uint8](base)]
+  rencoded[0] = (rencoded[0] shr 1) or (1 xor (c and 1)) shl (L - 1)
+  rencoded[1] = (rencoded[1] shr 1) or (1 xor c shr 1) shl (L - 1)
 
-type stranded* = tuple[enc:uint64, min_complement:uint8]
+proc rencode*(k: string): kmer {.inline.} =
+  ## reverse encode a string into a uint64
+  when not defined(danger):
+    if len(k) > 63:
+      quit "[kmer] can't encode kmer longer than 31"
+  let km1 = uint64(k.len - 1)
+  for base in k:
+    let c = lookup[cast[uint8](base)]
+    result[0] = (result[0] shl 1) or (1 xor (c and 1)) shl km1
+    result[1] = (result[1] shl 1) or (1 xor c shr 1) shl km1
+
+type stranded* = tuple[enc:kmer, min_complement:uint8]
 
 proc mincode*(k: string): stranded {.inline.} =
   ## encode a string into the min of itself and its reverse complement
   let f = k.encode()
-  let r = f.reverse_complement(k.len)
-  return (min(f, r), cast[uint8](r < f))
+  let r = k.rencode()
+  if f < r:
+    return (f, 0'u8)
+  return (r, 1'u8)
+
+proc decode*(e: kmer, k: var string) {.inline.} =
+  ## decode a string from a uint64 into k. the length
+  ## of k determines how much is decoded
+  for l in 0..<k.len:
+    k[k.len - 1 - l] = reverse_lookup[((e[1] shr l and 1) shl 1) or (e[0] shr l and 1)]
 
 iterator slide*(s:string, k: int): stranded {.inline.} =
   ## given a string (DNA seq) yield each possible kmer where
   ## the min of the foward and reverse complement is used.
   var f = s[0..<k].encode()
-  var r = f.reverse_complement(k)
+  var r = s[0..<k].rencode()
   var base:char
   for i in k..s.high:
     yield (min(f, r), cast[uint8](r < f))
@@ -101,8 +114,10 @@ iterator slide*(s:string, k: int): stranded {.inline.} =
     r.reverse_add(base, k)
   yield (min(f, r), cast[uint8](r < f))
 
+proc sum(m: seq[bool]): int {.inline.} =
+  for v in m: result += v.int
 
-iterator slide_forward*(s:string, k: int): uint64 {.inline.} =
+iterator slide_forward*(s:string, k: int): kmer {.inline.} =
   ## given a string (DNA seq) yield each possible kmer where
   ## the min of the foward and reverse complement is used.
   var f = s[0..<k].encode()
@@ -112,11 +127,7 @@ iterator slide_forward*(s:string, k: int): uint64 {.inline.} =
     f.forward_add(base, k)
   yield f
 
-
-proc sum(m: seq[bool]): int {.inline.} =
-  for v in m: result += v.int
-
-iterator slide_forward_mask*(s:string, k: int, mask: seq[bool]): uint64 {.inline.} =
+iterator slide_forward_mask*(s:string, k: int, mask: seq[bool]): kmer {.inline.} =
   ## take a boolean mask choosing which bases to extract. this is less
   ## efficient than slide_forward, bt allows sparse kmers
   assert k == sum(mask)
@@ -131,14 +142,16 @@ iterator slide_forward_mask*(s:string, k: int, mask: seq[bool]): uint64 {.inline
       if v: f.forward_add(s[i + j], k)
     yield f
 
-
-iterator dists*(s: string, k:int): auto {.inline.} =
-  ## yield each (min) encoded k-mer and its distance from the closest end of the read.
-  var i = 0
-  var m = s.len - k
-  for x in s.slide(k):
-    yield (min(i, m - i), x)
-    inc i
+proc reverse_complement*(encoded: uint64, L:int|uint64): uint64 {.inline.} =
+  ## fast reverse complement of encoded value
+  # from Zev: https://www.biostars.org/p/113640/#424280
+  result = not encoded
+  result = ((result shr 2'u64 and 0x3333333333333333'u64) or (result and 0x3333333333333333'u64) shl 2'u64);
+  result = ((result shr 4'u64 and 0x0F0F0F0F0F0F0F0F'u64) or (result and 0x0F0F0F0F0F0F0F0F'u64) shl 4'u64);
+  result = ((result shr 8'u64 and 0x00FF00FF00FF00FF'u64) or (result and 0x00FF00FF00FF00FF'u64) shl 8'u64);
+  result = ((result shr 16'u64 and 0x0000FFFF0000FFFF'u64) or (result and 0x0000FFFF0000FFFF'u64) shl 16'u64);
+  result = ((result shr 32'u64 and 0x00000000FFFFFFFF'u64) or (result and 0x00000000FFFFFFFF'u64) shl 32'u64);
+  return (result shr (2 * (32 - L)));
 
 
 when isMainModule:
@@ -148,157 +161,29 @@ when isMainModule:
 
   var t = cpuTime()
 
-  var k = "CTCCAGCCGGACGCGGCCGGCAGCAGACGCA"
+  var k = "CTCCAGCCGGACGCGGCCGGCAGCAGACGCACTCCAGCCGGACGCGGCCGGCAGCAGACGCA"
   var s = k
   var e = k.encode()
   e.decode(s)
-  var rc = e.reverse_complement(k.len)
-  rc.decode(s)
-  if s != "TGCGTCTGCTGCCGGCCGCGTCCGGCTGGAG":
-       echo "rev comp error"
-       quit(2)
 
-  e.forward_add('G', k.len)
+  echo k == s
+
+  echo k, " -> ", s
+
+  echo "add 'T'"
+  e.forward_add('T', k.len)
   e.decode(s)
-  if s != "TCCAGCCGGACGCGGCCGGCAGCAGACGCAG":
-    echo "error adding base:"
-    echo "got ", s
-    quit(1)
+  echo s
 
-  rc.reverse_add('A', k.len)
-  rc.reverse_add('A', k.len)
-  rc.decode(s)
-  if s != "TTTGCGTCTGCTGCCGGCCGCGTCCGGCTGG":
-    echo "error adding reverse base:"
-    echo "got ", s
-    quit(1)
+  echo "add reverse 'T'"
+  e = k.encode()
+  e.reverse_add('T', k.len)
+  e.decode(s)
+  echo s
 
-  #for i, s in pairs("TTTGCGTCTGCTGCCGGCCGCGTCCGGCTGG"):
-  #    echo i, s
-  k = k[0..<25]
-  s = k
-  proc reverse_complement_old*(encoded: uint64, L: int): uint64 {.inline.} =
-    ## reverse complement an encoded sequence where L is the kmer length.
-    var e = encoded
-    var base: uint8
-    for i in 0..<L:
-      base = 3'u8 - uint8(e and 3)
-      e = e shr 2
-      result = (result or base) shl 2
-    result = result shr 2
+  s.setLen(13)
+  echo "s:", k
+  for km in k.slide(13):
+    km.enc.decode(s)
+    echo s, " ", km.min_complement
 
-  echo "testing round-trip on random kmers"
-  for i in 0..200000:
-    shuffle(k)
-    e = k.encode()
-    e.decode(s)
-    rc = e.reverse_complement_old(s.len)
-    var rc2 = e.reverse_complement(s.len)
-    if s != k:
-       echo "error:", i
-       echo "error:", k
-       echo "error:", s
-       quit(2)
-    if rc != rc2:
-      echo "error:", i, " k:", k, " s:", s
-
-  echo cpuTime() - t
-
-  echo "testing old reverse_complement"
-  let ntimes = 10_000_000
-  var n = 0
-  var encs = newSeq[uint64](ntimes)
-  for i in 0..<ntimes:
-    shuffle(k)
-    encs.add(k.encode())
-
-  var old = newSeqOfCap[uint64](ntimes)
-  t = cpuTime()
-  for e in encs:
-    rc = e.reverse_complement_old(s.len)
-    if rc >= 69219000000000'u64:
-      n.inc
-  # old.add(rc)
-  echo "time:", cpuTime() - t, " n:", n, " of ", ntimes
-
-  echo "testing new reverse_complement"
-  t = cpuTime()
-  n = 0
-  for i, e in encs:
-    rc = e.reverse_complement(s.len)
-    if rc >= 69219000000000'u64:
-      n.inc
-  #doAssert old[i] == rc
-  echo "time:", cpuTime() - t, " n:", n, " of ", ntimes
-
-
-  block:
-    var s = "ACTGGC"
-  #[
-
-  s = "TTTGCGTCTGCTGCCGGCCGCGTCCGGCTGG"
-  var space = ""
-  var kx1 = new_string(10)
-  var kx2 = new_string(10)
-  var i = 0
-  for d, u in s.dists(10):
-    echo s
-    u.decode(kx1)
-    u.reverse_complement(10).decode(kx2)
-    assert kx1 == s[i..<(i+10)] or kx2 == s[i..<(i+10)]
-    if kx1 == s[i..<(i+10)]:
-      echo space & kx1, " ", d
-    else:
-      echo space & kx2, " ", d
-    space &= " "
-    echo ""
-    i += 1
-  ]#
-
-
-  block:
-
-    var s = "ACACACACACT"
-    var k = 3
-    var mask = @[true, false, true, false, true]
-    echo "slide mask:", s, " mask:", mask
-    for v in s.slide_forward_mask(k, mask):
-      var n = newString(k)
-      v.decode(n)
-      echo n
-
-    mask = @[true, true, false, false, false, true]
-    echo "mask:", mask
-    for v in s.slide_forward_mask(k, mask):
-      var n = newString(k)
-      v.decode(n)
-      echo n
-
-
-
-  block:
-    var s = "ACACACACACT"
-    var e = s.encode
-    var d = newString(s.len)
-    (not e).decode(d)
-    echo s
-    echo d
-
-
-  block:
-    var s = "ACTGACGGACCCGAGGGCACCCGAGGCCTTTTTTTTGCGGGAGGAGGAGACTGACTGCGGGAGGAGGAGACTGACTGCGGGAGGAGGAGACTGACTGCGGGAGGAGGAG"
-    var t = cpuTime()
-
-    var lastS = 0'u64
-    for i in 0..600_000:
-      var S = 0'u64
-      for k in s.slide(25):
-        S += k.enc mod 15
-      if i > 0:
-        doAssert S == lastS
-      else:
-        lastS = S
-        echo lastS
-        var lastS = S
-
-    echo "slide time:",  cpuTime() - t
