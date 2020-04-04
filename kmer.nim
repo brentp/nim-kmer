efabc6ac96ff31b9b26e3b18ddc31fc51e86e617
Brent Pedersen <bpederse@gmail.com>
2020-04-03 19:32:05 -0600

    changes

--- kmer.nim
+++ kmer.nim
@@ -48,10 +48,10 @@ proc `<`*(a:kmer, b:kmer): bool =
 proc min*(a:kmer, b:kmer): kmer =
   {.emit: "`result` = ((uint128)(`a`) < (uint128)(`b`)) ? `a` : `b`;" .}
 
-template mask(k:int): uint64 =
-  (1'u64 shl k) - 1
+template mask(k:uint64): uint64 =
+  (1'u64 shl k) - 1'u64
 
-proc forward_add*(encoded: var kmer, base: char, L: int) {.inline.} =
+proc forward_add*(encoded: var kmer, base: char, L: uint64) {.inline.} =
   ## drop the first base from an encoded kmer of length L and add a new one.
   ## useful for sliding along a sequence.
   let mask = L.mask
@@ -61,7 +61,7 @@ proc forward_add*(encoded: var kmer, base: char, L: int) {.inline.} =
 
 proc encode*(k: string): kmer {.inline.} =
   ## encode a string into a uint64
-  let mask = k.len.mask
+  let mask = k.len.uint64.mask
   when not defined(danger):
     if len(k) > 63:
       quit "[kmer] can't encode kmer longer than 31"
@@ -70,7 +70,7 @@ proc encode*(k: string): kmer {.inline.} =
     result[0] = ((result[0] shl 1) or (c and 1)) and mask
     result[1] = ((result[1] shl 1) or (c shr 1)) and mask
 
-proc reverse_add*(rencoded: var kmer, base: char, L: int) {.inline.} =
+proc reverse_add*(rencoded: var kmer, base: char, L: uint64) {.inline.} =
   ## drop the last base from a rev-comped kmer of length L and add a new
   ## complemented base at the start.
   let c = lookup[cast[uint8](base)]
@@ -105,24 +105,24 @@ proc decode*(e: kmer, k: var string) {.inline.} =
     k[k.len - 1 - l] = reverse_lookup[((e[1] shr l and 1) shl 1) or (e[0] shr l and 1)]
 
 
-iterator slide*(s:string, k: int): stranded {.inline.} =
+iterator slide*(s:string, k: uint64): stranded {.inline.} =
   ## given a string (DNA seq) yield each possible kmer where
   ## the min of the foward and reverse complement is used.
   let pre = s[0..<k]
   var f = pre.encode()
   var r = pre.rencode()
   var base:char
-  for i in k..s.high:
+  for i in k..s.high.uint64:
 
-    if r < f:
-      yield (r, 1'u8)
-    else:
-      yield (f, 0'u8)
+    #if r < f:
+    #  yield (r, 1'u8)
+    #else:
+    yield (f, 0'u8)
     #yield (min(f, r), cast[uint8](r < f))
     base = s[i]
     f.forward_add(base, k)
     r.reverse_add(base, k)
-  if r < f:
+  if r[0] < f[0]:
     yield (r, 1'u8)
   else:
     yield (f, 0'u8)
@@ -130,31 +130,6 @@ iterator slide*(s:string, k: int): stranded {.inline.} =
 
 proc sum(m: seq[bool]): int {.inline.} =
   for v in m: result += v.int
-
-iterator slide_forward*(s:string, k: int): kmer {.inline.} =
-  ## given a string (DNA seq) yield each possible kmer where
-  ## the min of the foward and reverse complement is used.
-  var f = s[0..<k].encode()
-  for i in k..s.high:
-    yield f
-    let base:char = s[i]
-    f.forward_add(base, k)
-  yield f
-
-iterator slide_forward_mask*(s:string, k: int, mask: seq[bool]): kmer {.inline.} =
-  ## take a boolean mask choosing which bases to extract. this is less
-  ## efficient than slide_forward, bt allows sparse kmers
-  assert k == sum(mask)
-  var sequence = newStringOfCap(k)
-  for i, v in mask:
-    if v: sequence.add(s[i])
-
-  var f = sequence.encode()
-  yield f
-  for j in 1..s.high - mask.high:
-    for i, v in mask:
-      if v: f.forward_add(s[i + j], k)
-    yield f
 
 proc reverse_complement*(encoded: uint64, L:int|uint64): uint64 {.inline.} =
   ## fast reverse complement of encoded value
@@ -169,7 +144,6 @@ proc reverse_complement*(encoded: uint64, L:int|uint64): uint64 {.inline.} =
 
 proc reverse_complement*(encoded: kmer, L:int|uint64): kmer {.inline.} =
   ## fast reverse complement of encoded value
-  # from Zev: https://www.biostars.org/p/113640/#424280
   result[1] = encoded[0].reverse_complement(L)
   result[0] = encoded[1].reverse_complement(L)
 
@@ -196,7 +170,7 @@ when isMainModule:
   echo "rc:", rs
 
   echo "add 'T'"
-  e.forward_add('T', k.len)
+  e.forward_add('T', k.len.uint64)
   e.decode(s)
   echo s
 
@@ -205,8 +179,8 @@ when isMainModule:
   s.setLen(k.len)
   var rc = k.rencode()
   echo "add reverse 'A'*2"
-  rc.reverse_add('A', k.len)
-  rc.reverse_add('A', k.len)
+  rc.reverse_add('A', k.len.uint64)
+  rc.reverse_add('A', k.len.uint64)
   rc.decode(s)
   echo "rev:", s
 
